629b83a06878e65da705c228f1c284a776e9f1d4
SHASHANK <primiumshukla474@gmail.com>
2025-01-08 02:54:56 +0530

    Update __main__.py

--- AvishaRobot/__main__.py
+++ AvishaRobot/__main__.py
@@ -206,7 +206,7 @@ CHAT_SETTINGS = {}
 USER_SETTINGS = {}
 
 for module_name in ALL_MODULES:
-    imported_module = importlib.import_module("MukeshRobot.modules." + module_name)
+    imported_module = importlib.import_module("AvishaRobot.modules." + module_name)
     if not hasattr(imported_module, "__mod_name__"):
         imported_module.__mod_name__ = imported_module.__name__
 
