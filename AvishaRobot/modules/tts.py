c17732d461109834b8ee3c655a86717fc0ef92ca
SHASHANK <primiumshukla474@gmail.com>
2025-01-08 04:07:23 +0530

    Update tts.py

--- AvishaRobot/modules/tts.py
+++ AvishaRobot/modules/tts.py
@@ -6,7 +6,7 @@ from AvishaRobot import pbot as app
 def text_to_speech(client, message):
     text = message.text.split(' ', 1)[1]
     tts = gTTS(text=text, lang='hi')
-    tts.save('avisha.mp3')
+    tts.save('senorita.mp3')
     client.send_audio(message.chat.id, 'avisha.mp3')
   
 __mod_name__ = "ɢᴛᴛs"
