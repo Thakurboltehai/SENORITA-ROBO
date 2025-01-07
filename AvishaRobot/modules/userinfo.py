35d5ec6f21b5eed9cfcdc78167747c1d05c542f6
SHASHANK <primiumshukla474@gmail.com>
2025-01-08 05:09:40 +0530

    Update userinfo.py

--- AvishaRobot/modules/userinfo.py
+++ AvishaRobot/modules/userinfo.py
@@ -271,7 +271,7 @@ def info(update: Update, context: CallbackContext):
         disaster_level_present = True
 
     if disaster_level_present:
-        text += ' \n⬤ [<a href="https://t.me/RoY_Editx/881">ᴄʟɪᴄᴋ ʜᴇʀᴇ ᴛᴏ ᴋɴᴏᴡ ᴡʜᴀᴛ ɪs ᴅɪsᴀsᴛᴇʀ ʟᴇᴠᴇʟs.</a>]'.format(
+        text += ' \n⬤ [<a href="https://t.me/SHIVANSH474">ᴄʟɪᴄᴋ ʜᴇʀᴇ ᴛᴏ ᴋɴᴏᴡ ᴡʜᴀᴛ ɪs ᴅɪsᴀsᴛᴇʀ ʟᴇᴠᴇʟs.</a>]'.format(
             bot.username
         )
 
