f10169640723bae68b8af75af0583510cc3e6fe2
SHASHANK <primiumshukla474@gmail.com>
2025-01-08 06:01:00 +0530

    Update mongo.py

--- AvishaRobot/Love/mongo.py
+++ AvishaRobot/Love/mongo.py
@@ -6,7 +6,7 @@ from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 from AvishaRobot import MONGO_DB_URI
 
 mongo = MongoCli(MONGO_DB_URI)
-db = mongo.AvishaRobot
+db = mongo.MukeshRobot
 
 coupledb = db.couple
 
