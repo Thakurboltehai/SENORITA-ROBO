a957a5614e8722c7e5bf11f5e7a84ff23e41f83a
SHASHANK <primiumshukla474@gmail.com>
2025-01-08 05:44:42 +0530

    Update __init__.py

--- AvishaRobot/database/__init__.py
+++ AvishaRobot/database/__init__.py
@@ -4,7 +4,7 @@ from async_pymongo import AsyncClient
 
 from AvishaRobot import MONGO_DB_URI
 
-DBNAME = "AvishaRobot"
+DBNAME = "MukeshRobot"
 
 mongo = AsyncClient(MONGO_DB_URI)
 dbname = mongo[DBNAME]
