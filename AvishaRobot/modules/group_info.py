a53b0f4ab24a44468766c1b92d75047be9506b86
SHASHANK <primiumshukla474@gmail.com>
2025-01-10 04:09:35 +0530

    Update group_info.py

--- AvishaRobot/modules/group_info.py
+++ AvishaRobot/modules/group_info.py
@@ -4,7 +4,7 @@ from AvishaRobot import pbot as app
 
 EVAA = [
     [
-        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/StrangerSuperbot?startgroup=true"),
+        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/vzsrobot?startgroup=true"),
     ],
 ]
 
