72a0e469433532776621f14fdb5471d650ed8af8
SHASHANK <primiumshukla474@gmail.com>
2025-01-10 04:09:46 +0530

    Update hastag.py

--- AvishaRobot/modules/hastag.py
+++ AvishaRobot/modules/hastag.py
@@ -7,7 +7,7 @@ from pyrogram import filters
 
 EVAA = [
     [
-        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/StrangerSuperbot?startgroup=true"),
+        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/vzsrobot?startgroup=true"),
     ],
 ]
 
